9561feae7f04ffa19e711932229a70485aaf0a7a
Henchenski <64487772+Anluhen@users.noreply.github.com>
2020-07-04 18:15:54 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,25 @@
+var x
+var vx
+var i
+
 function setup(){
   createCanvas(600,600)
-  
-  var x = width/2
-  var vx = 1
+
+  x = 400
+  vx = 1
+  i = 0
 }
 
 function draw() {
-  vx = 1 - (x/width/2)
-  x = x + vx
-  
+  background(150)
+  vx = 5*sin(i)
+  x = x - vx
+
   ellipse(x,height/2,20)
+
+  if (i < 2*PI) {
+    i = i + 0.05
+  }else {
+    i = 0
+  }
 }
