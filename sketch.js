bf74dddb2e11ee37ecad2a11fed77d831289c3c2
Henchenski <64487772+Anluhen@users.noreply.github.com>
2020-07-04 17:52:57 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,24 +1,15 @@
-var paragraph
-var textbox
-var slider
+var canvas
 
 function setup(){
-  noCanvas()
-  textbox = createInput('enter text')
-  slider = createSlider(10,64,15)
-  paragraph = createP('Paragraph')
-
-  textbox.input(updateText)
-  slider.input(updateSize)
-}
-
-function updateSize(){
-  paragraph.style("font-size", slider.value() + "pt")
-}
-
-function updateText(){
-  paragraph.html(textbox.value())
+  canvas = createCanvas(600,600)
+  
+  var x = width/2
+  var vx = 1
 }
 
 function draw() {
+  vx = 1 - (x/width/2)
+  x = x + vx
+  
+  ellipse(x,height/2,20)
 }
